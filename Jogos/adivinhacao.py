ce7ad69a1b18411b572d719dc77c970ab987d813
riff-imaginario <lucas.aleph.101@gmail.com>
2019-07-29 14:39:23 -0300

    add loop while

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -3,19 +3,25 @@ print("Bem-vindo ao jogo de adivinhação!")
 print('*********************************')
 
 numero_secreto = 42
+tentativas     = 3
+rodada         = 1
 
-chute = int(input('Digite o seu número: '))
+while(rodada <= tentativas):
+    print('Tentativas:', rodada, 'de', tentativas)
+    chute = int(input('Digite o seu número: '))
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-if(acertou):
-    print('Você acertou')
-else:
-    if(maior):
-        print('Você errou! Seu número foi maior do que o número secreto.')
-    elif(chute < numero_secreto):
-        print('Você errou! Seu número foi menor do que o número secreto.')
+    if(acertou):
+        print('Você acertou')
+    else:
+        if(maior):
+            print('Você errou! Seu número foi maior do que o número secreto.')
+        elif(chute < numero_secreto):
+            print('Você errou! Seu número foi menor do que o número secreto.')
+
+    rodada     += 1
 
 print('Fim do jogo')
