f9775d9b620616ad75cc02fdd8bbc2898c292f05
riff-imaginario <lucas.aleph.101@gmail.com>
2019-07-29 13:43:28 -0300

    add condition to guessing number

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -1,1 +1,12 @@
+print('*********************************')
 print("Bem-vindo ao jogo de adivinhação!")
+print('*********************************')
+
+numero_secreto = 42
+
+chute = int(input('Digite o seu número: '))
+
+if(numero_secreto == chute):
+    print('Você acertou')
+else:
+    print('Você errou')
