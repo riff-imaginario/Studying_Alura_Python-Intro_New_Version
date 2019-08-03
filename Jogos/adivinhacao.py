06d86b74ef81d703a338de0f5d5dba415825a82b
riff-imaginario <lucas.aleph.101@gmail.com>
2019-08-03 16:50:35 -0300

    changes while for for loop and add break and continue

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -4,11 +4,14 @@ print('*********************************')
 
 numero_secreto = 42
 tentativas     = 3
-rodada         = 1
 
-while(rodada <= tentativas):
+for rodada in range(1, tentativas + 1):
     print('Tentativas: {0} de {1}'.format(rodada, tentativas))
-    chute = int(input('Digite o seu número: '))
+    chute = int(input('Digite um número entre 1 e 100: '))
+
+    if(chute < 1 or chute > 100):
+        print('Você deve digitar um número entre 1 e 100!')
+        continue  # volta para o início do loop
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -16,12 +19,11 @@ while(rodada <= tentativas):
 
     if(acertou):
         print('Você acertou')
+        break  # sai do loop
     else:
         if(maior):
             print('Você errou! Seu número foi maior do que o número secreto.')
         elif(chute < numero_secreto):
             print('Você errou! Seu número foi menor do que o número secreto.')
-
-    rodada     += 1
 
 print('Fim do jogo')
