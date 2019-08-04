04038f34570d0d59ba0615eaebb2f7cadbbb7358
riff-imaginario <lucas.aleph.101@gmail.com>
2019-08-03 23:22:38 -0300

    add loop to forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,6 +3,21 @@ def jogar():
     print("Bem-vindo ao jogo de forcaforca.py!")
     print('*********************************')
 
+    palavra_secreta = 'banana'
+
+    enforcou = False
+    acertou  = False
+
+    while(not enforcou and not acertou):
+        chute = input('Qual letra? ')
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print('Encontrei a letra {} na posição {}'.format(letra, index))
+            index += 1
+
+
     print('Fim do jogo')
 
 
